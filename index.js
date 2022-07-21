58650f212a15da44293d52e3bdbf96834f5181cb
mnaveeduddin <mnaveeduddin.kpt@gmail.com>
2022-07-21 11:45:38 +0530

    removed buttons appearing while printing screen

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-function addTotal(){
-    let num1 = parseInt(document.getElementById("inspectioncharge").value);
-    let num2 = parseInt(document.getElementById("servicecharge").value);
-    let num3 = parseInt(document.getElementById("parts").value);
-    let num4 = parseInt(document.getElementById("labour").value);
-    document.getElementById("total").innerHTML = num1 + num2 + num3 + num4;
+function addTotal() {
+  let num1 = parseInt(document.getElementById("inspectioncharge").value);
+  let num2 = parseInt(document.getElementById("servicecharge").value);
+  let num3 = parseInt(document.getElementById("parts").value);
+  let num4 = parseInt(document.getElementById("labour").value);
+  document.getElementById("total").innerHTML = num1 + num2 + num3 + num4;
 }
 
-function downloadPDF(){
-    var name = document.getElementById('contact-name').value;
+function downloadPDF() {
+  var name = document.getElementById("contact-name").value;
 
-    // For current date;
-    Date.prototype.today = function () { 
-        return ((this.getDate() < 10)?"0":"") + this.getDate() +"-"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"-"+ this.getFullYear();
-    }
+  // For current date;
+  Date.prototype.today = function () {
+    return (
+      (this.getDate() < 10 ? "0" : "") +
+      this.getDate() +
+      "-" +
+      (this.getMonth() + 1 < 10 ? "0" : "") +
+      (this.getMonth() + 1) +
+      "-" +
+      this.getFullYear()
+    );
+  };
 
-    // For current time;
-    Date.prototype.timeNow = function () {
-         return ((this.getHours() < 10)?"0":"") + this.getHours() +"-"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
-    }
+  // For current time;
+  Date.prototype.timeNow = function () {
+    return (
+      (this.getHours() < 10 ? "0" : "") +
+      this.getHours() +
+      "-" +
+      (this.getMinutes() < 10 ? "0" : "") +
+      this.getMinutes()
+    );
+  };
 
-    var newDate = new Date();
-    var datetime = newDate.today() + " - " + newDate.timeNow();
-    
-    var element = document.getElementById('exportToPDF');
-    var opt = {
-        margin:       0.2,
-        filename:     name + "-" + datetime,
-        image:        { type: 'jpg', quality: 1 },
-        html2canvas:  { scale: 1 },
-        jsPDF:        { unit: 'in', format: 'ledger', orientation: 'portrait' }
-      }; 
+  var newDate = new Date();
+  var datetime = newDate.today() + " - " + newDate.timeNow();
 
-      html2pdf().set(opt).from(element).save();
+  var element = document.getElementById("exportToPDF");
+  var opt = {
+    margin: 0.2,
+    filename: name + "-" + datetime,
+    image: { type: "jpg", quality: 1 },
+    html2canvas: { scale: 1 },
+    jsPDF: { unit: "in", format: "ledger", orientation: "portrait" },
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
+
+function printScreen(divName) {
+  var printContents = document.getElementById(divName).innerHTML;
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+
+  window.print();
+
+  document.body.innerHTML = originalContents;
 }
